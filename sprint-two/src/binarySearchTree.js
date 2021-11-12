711c8eccdb441c1cd1bcb728f094b57dc5ce5a85
David Rajec <david.rajec@gmail.com>
2021-11-11 16:00:40 -0800

    Finish bare minimum requirements minus complexity analysis

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,68 @@
 var BinarySearchTree = function(value) {
+  var binarySearchTree = Object.create(bstPrototype);
+  binarySearchTree.value = value;
+  binarySearchTree.left = null;
+  binarySearchTree.right = null;
+  binarySearchTree.nodes = {};
+  return binarySearchTree;
+};
+
+var bstPrototype = {};
+
+bstPrototype.insert = function(newValue) {
+  let newTree = BinarySearchTree(newValue);
+
+  const recurThroughTree = function(treeNode) {
+    if (treeNode.left === null && newTree.value < treeNode.value) {
+      treeNode.left = newTree;
+    } else if (treeNode.right === null && newTree.value >= treeNode.value) {
+      treeNode.right = newTree;
+    } else {
+      if (newTree.value < treeNode.value) {
+        recurThroughTree(treeNode.left);
+      } else {
+        recurThroughTree(treeNode.right);
+      }
+    }
+  };
+  recurThroughTree(this);
+};
+
+bstPrototype.contains = function(target) {
+  let result = false;
+  const searchThroughTree = function(treeNode, target) {
+    if (treeNode === null) {
+      return;
+    } else if (treeNode.value === target) {
+      result = true;
+    } else {
+      if (target < treeNode.value) {
+        searchThroughTree(treeNode.left, target);
+      } else {
+        searchThroughTree(treeNode.right, target);
+      }
+    }
+  };
+  searchThroughTree(this, target);
+  return result;
+};
+
+bstPrototype.depthFirstLog = function(cb) {
+  const depthFirst = function(treeNode, cb) {
+    if (treeNode === null) {
+      return;
+    }
+    cb(treeNode.value);
+    depthFirst(treeNode.left, cb);
+    depthFirst(treeNode.right, cb);
+  };
+  depthFirst(this, cb);
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ Insert: O(N)
+ Contains: O(log(N))
+ DepthFirstLog: O(N)
  */
